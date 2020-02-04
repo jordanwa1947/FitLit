835c98a8b336ffa5b468e940ca4417cff139eedb
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-04 16:43:18 -0700

    Add method to calculate the average step goal of all users

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -14,6 +14,13 @@ class UserRepository {
   getUserData(id) {
     return this.users.find(user => user.id === id);
   }
+
+  calculateAvgStepGoalOfUsers() {
+    var totalDailySteps = this.users.reduce((acc, user) => {
+      return acc + user.dailyStepGoal;
+    }, 0);
+    return totalDailySteps / this.users.length;
+  }
 }
 
 if (typeof module !== 'undefined') {
