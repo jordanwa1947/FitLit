553267a9a1a27dc699d508d07c3f7341e5115440
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-04 16:28:54 -0700

    Create userRepository class to include array of users

---
+++ src/UserRepository.js
@@ -0,0 +1,22 @@
+const User = require('../src/User');
+
+
+class UserRepository {
+  constructor(dataAttributes) {
+    this.users = this.generateUsers(dataAttributes);
+  }
+
+  generateUsers(dataAttributes) {
+    return dataAttributes.map(data => {
+      return new User(data);
+    })
+  }
+
+  // getUserData() {
+  //
+  // }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = UserRepository;
+}
