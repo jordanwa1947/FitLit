189ea83cdd4f83711316ca92ec0cf437075fc167
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-04 18:57:35 -0700

    Create method to calculate the average amount of fluids consumed per day for all time per user

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,6 +1,16 @@
 class Hydration {
-  constructor() {
+  constructor(hydrationData) {
+    this.hydrationData = hydrationData;
+  }
 
+  calcAvgOuncesConsuedForAllTime(id) {
+    let hydrationPerUser = this.hydrationData.filter(hydration => {
+      return hydration.userID === id;
+    });
+    let totalHydration = hydrationPerUser.reduce((acc, hydration) => {
+      return acc + hydration.numOunces
+    }, 0)
+    return totalHydration / hydrationPerUser.length;
   }
 }
 
