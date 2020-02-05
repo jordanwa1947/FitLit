f3d33ae4b6183b55533dd34ea827a5f6f87af77c
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-05 07:26:05 -0700

    Implements returnSleepDataField and findUserSleepData methods

--- src/Sleep.js
+++ src/Sleep.js
@@ -3,6 +3,16 @@ class Sleep {
     this.sleepData = sleepData;
   }
 
+  findUserSleepData(id, date) {
+    return this.sleepData.find((sleepData) => {
+      return sleepData.date === date;
+    });
+  }
+
+  returnSleepDataField(id, date, field) {
+    return this.findUserSleepData(id, date)[field];
+  }
+
   findAverge(id, field) {
     let sum = 0;
     let numberOfRecords = 0;
