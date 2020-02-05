a8e15529e9e3d705982415d026fc3a4e7303daca
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-04 18:32:07 -0700

    Add to test to ensure Hydration is a function

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,7 +2,65 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Hydration = require('../src/Hydration');
+let users, hydration, hydrationData;
+
+beforeEach(()=> {
+  users = [{
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  }];
+
+  hydrationData = [{
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 69
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 91
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  }];
+
+  hydration = new Hydration(hydrationData);
+})
 
 describe('Hydration', function() {
+  it('should be a function', function() {
+    expect(hydration).to.be.an.instanceOf(Hydration);
+  });
 
+  it()
 });
