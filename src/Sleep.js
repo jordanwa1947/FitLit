b4883329e85524e04da2704dbd59ec879a3c1ca7
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-05 09:28:21 -0700

    Implements the findAvergeForAUserOverAWeek method and refactors old methods to use make code drier

--- src/Sleep.js
+++ src/Sleep.js
@@ -9,20 +9,35 @@ class Sleep {
     });
   }
 
+  findAverge(records, field) {
+    const sum = records.reduce((sum, data) => {
+      return sum + data[field]
+    }, 0);
+    return sum / records.length || 0;
+  }
+
   returnSleepDataField(id, date, field) {
     return this.findUserSleepData(id, date)[field];
   }
 
-  findAverge(id, field) {
-    let sum = 0;
-    let numberOfRecords = 0;
-    this.sleepData.forEach((data) => {
-      if(data.userID === id) {
-        sum += data[field];
-        numberOfRecords++;
-      }
-    });
-    return sum / numberOfRecords || 0;
+  findAvergeForAUser(id, field) {
+    const records = this.sleepData.filter((data) => data.userID === id);
+    return this.findAverge(records, field);
+  }
+
+  findSleepDataForAGivenWeek(date) {
+    let currentDate = new Date(date).getTime();
+    const weekInMilliseconds = 604800000;
+    const lastWeek = currentDate - weekInMilliseconds;
+    return this.sleepData.filter((data) => {
+      let dataDate = new Date(data.date).getTime();
+      return dataDate > lastWeek && dataDate <= currentDate;
+    })
+  }
+
+  findAvergeForAUserOverAWeek(date, field) {
+    var records = this.findSleepDataForAGivenWeek(date);
+    return this.findAverge(records, field);
   }
 }
 
