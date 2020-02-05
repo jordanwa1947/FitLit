c5e0b95deb0c9b82a00fdc5980c202baa0b7e62b
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-05 10:45:14 -0700

    Adds test for findUsersWithAvgGreaterThanThree method

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -81,11 +81,11 @@ describe('Sleep', function() {
   });
 
   it('should be able to calculate the average sleep for a user', function() {
-    expect(sleep.findAvergeForAUser(2, 'hoursSlept')).to.equal(8.899999999999999);
+    expect(sleep.findAverageForAUser(2, 'hoursSlept')).to.equal(8.899999999999999);
   });
 
   it('should be able to calculate the average sleep quality for a user', function() {
-    expect(sleep.findAvergeForAUser(2, 'sleepQuality')).to.equal(5.200000000000001);
+    expect(sleep.findAverageForAUser(2, 'sleepQuality')).to.equal(5.200000000000001);
   });
 
   it('should be able to find how many hours a user slept for a certain date', function() {
@@ -106,11 +106,37 @@ describe('Sleep', function() {
   });
 
   it('should be able to find the average hours slept over a week', function() {
-    expect(sleep.findAvergeForAUserOverAWeek('2019/06/15', 'hoursSlept')).to.equal(7.8375);
+    expect(sleep.findAverageForAUserOverAWeek('2019/06/15', 'hoursSlept')).to.equal(7.8375);
   });
 
   it('should be able to find the average sleep quality over a week', function() {
-    expect(sleep.findAvergeForAUserOverAWeek('2019/06/15', 'sleepQuality')).to.equal(4.6375);
+    expect(sleep.findAverageForAUserOverAWeek('2019/06/15', 'sleepQuality')).to.equal(4.6375);
+  });
+
+  it('should be able to find the average sleep quality over all users', function() {
+    expect(sleep.findAverageForAllUsers('sleepQuality')).to.equal(4.927272727272729);
+  });
+
+  it('should be able to find users who had an average sleep quality greater than three during a week', function() {
+    users = [{
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [16,4,8]
+    },
+    {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [9,18,24,19]
+    }]
+    expect(sleep.findUsersWithAvgGreaterThanThree(users, 'sleepQuality', '2019/06/15')).to.deep.equal([users[1]]);
   });
 
 });
