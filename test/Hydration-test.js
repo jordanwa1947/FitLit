97f6251f1f102fec394837203c27379564737bf2
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-04 19:14:52 -0700

    Remove variable for users in beforeEach hook

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,37 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Hydration = require('../src/Hydration');
-let users, hydration, hydrationData;
+let hydration, hydrationData;
 
 beforeEach(()=> {
-  users = [{
-    "id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Jarvis Considine",
-    "address": "30086 Kathryn Port, Ciceroland NE 07273",
-    "email": "Dimitri.Bechtelar11@gmail.com",
-    "strideLength": 4.5,
-    "dailyStepGoal": 5000,
-    "friends": [
-      9,
-      18,
-      24,
-      19
-    ]
-  }];
-
   hydrationData = [{
     "userID": 1,
     "date": "2019/06/15",
