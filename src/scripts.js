f87d51adff1fad83269e359df18218bdf97ac666
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-06 15:00:11 -0700

    Display user info on the page

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,13 @@
 const userRepository = new UserRepository(userData);
+insertUserInfo();
+
+function insertUserInfo() {
+  const user = new User(userRepository.users[0]);
+  const title = `Let's get fit, ${user.getUserFirstName()}!`
+  document.getElementById('get-fit-title').innerText = title;
+  document.getElementById('user-name').innerText = user.name;
+  document.getElementById('user-id').innerText = user.id;
+  document.getElementById('user-address').innerText = user.address;
+  document.getElementById('user-stride-length').innerText = user.strideLength;
+  document.getElementById('daily-step-goal').innerText = user.dailyStepGoal;
+}
