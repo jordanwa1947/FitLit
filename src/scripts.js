805d423820e0ba636a0f0578f1292799e4dc9fa0
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-06 15:07:32 -0700

    Adds labels for user info and a comparison to the average user info of a user

--- src/scripts.js
+++ src/scripts.js
@@ -5,9 +5,10 @@ function insertUserInfo() {
   const user = new User(userRepository.users[0]);
   const title = `Let's get fit, ${user.getUserFirstName()}!`
   document.getElementById('get-fit-title').innerText = title;
-  document.getElementById('user-name').innerText = user.name;
-  document.getElementById('user-id').innerText = user.id;
-  document.getElementById('user-address').innerText = user.address;
-  document.getElementById('user-stride-length').innerText = user.strideLength;
-  document.getElementById('daily-step-goal').innerText = user.dailyStepGoal;
+  document.getElementById('user-name').innerText = `Username: ${user.name}`;
+  document.getElementById('user-id').innerText = `User Id: ${user.id}`;
+  document.getElementById('user-address').innerText = `Address: ${user.address}`;
+  document.getElementById('user-stride-length').innerText = `Stride Length: ${user.strideLength}`;
+  document.getElementById('daily-step-goal').innerText = `Step Goal: ${user.dailyStepGoal}`;
+  document.getElementById('avg-step-goal').innerText = `Average Step Goal: ${userRepository.calculateAvgStepGoalOfUsers()}`;
 }
