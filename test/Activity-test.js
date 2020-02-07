db8973c725fcc13e92f29d9bd71b737906efc677
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-06 18:41:42 -0700

    Create test assertion for returning the number a miles a user walked on a specific day based on their numnber of steps

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const User = require('../src/User');
 const Activity = require('../src/Activity');
-let activityData, activity;
+let activityData, user, activity;
 
 beforeEach(() => {
   activityData = [
@@ -21,6 +22,36 @@ beforeEach(() => {
       "flightsOfStairs": 13
     }
   ];
+
+  user = new User({
+    "id": 4,
+    "name": "Mae Connelly",
+    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    "email": "Marcos_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Erick Schaden",
+    "address": "514 Mayert Walk, Jordaneside SC 55023-6523",
+    "email": "Vanessa_Gerhold@gmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 8000,
+    "friends": [
+      13,
+      44,
+      49,
+      33,
+      10
+    ]
+  })
   activity = new Activity(activityData);
 });
 
@@ -33,8 +64,9 @@ describe('Activity', function() {
     expect(activity.activityData).to.deep.equal(activityData);
   });
 
-  it.skip('should return the number of miles a user walked for a specific day based on on their number of steps', function() {
-
+  it('should return the number of miles a user walked for a specific day based on on their number of steps', function() {
+    expect(activity.getMilesWalkedForDay(activityData[0], user[0])).to.equal(2.0);
+    expect(activity.getMilesWalkedForDay(activityData[1], user[1])).to.equal(6.7);
   });
 
   it.skip('should return how many minutes a user was active for a given day, specified by the date', function() {
