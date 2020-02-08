2703a23c394a28e28506722beae6f7e0a1d5ec7c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-07 18:59:21 -0700

    Pass test to return the total minutes a user was active for a given day

--- src/Activity.js
+++ src/Activity.js
@@ -4,20 +4,24 @@ class Activity {
   }
 
   findAllUserActivities(id, date) {
-    let activityPerUser = this.activityData.filter(activity => {
+    const activityPerUser = this.activityData.filter(activity => {
       return activity.userID === id;
     });
 
-    let activityPerDate = activityPerUser.find(activity => {
+    const activityPerDate = activityPerUser.find(activity => {
       return activity.date === date;
     });
     return activityPerDate;
   }
 
   getMilesWalkedForDay(id, date, stride) {
-    this.findAllUserActivities(id, date)
     return Number(((this.findAllUserActivities(id, date).numSteps * stride) / 5280).toFixed(1));
   }
+
+  getMinutesActive(id, date) {
+    return this.findAllUserActivities(id, date).minutesActive;
+  }
+
 }
 
 
