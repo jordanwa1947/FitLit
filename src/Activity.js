95e2f1028d24dbcdc2caacc6c34d0de5fb4e0805
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-07 17:37:40 -0700

    Refactor getMilesWalkedForDay method to take in a function that finds all users data for a given date

--- src/Activity.js
+++ src/Activity.js
@@ -3,18 +3,23 @@ class Activity {
     this.activityData = activityData;
   }
 
+  findAllUserActivities(id, date) {
+    let activityPerUser = this.activityData.filter(activity => {
+      return activity.userID === id;
+    });
+
+    let activityPerDate = activityPerUser.find(activity => {
+      return activity.date === date;
+    });
+    return activityPerDate;
+  }
+
   getMilesWalkedForDay(id, date, stride) {
-  let activityPerUser = this.activityData.filter(activity => {
-    return activity.userID === id;
-  });
-
-  let activityPerDate = activityPerUser.find(activity => {
-    return activity.date === date;
-  });
-
-  return Number(((activityPerDate.numSteps * stride) / 5280).toFixed(1));
+    this.findAllUserActivities(id, date)
+    return Number(((this.findAllUserActivities(id, date).numSteps * stride) / 5280).toFixed(1));
   }
 }
+
 
 if (typeof module !== 'undefined') {
   module.exports = Activity;
