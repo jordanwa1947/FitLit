85eb7a3236a8e441086d21c7f4b866392c4f91c3
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-07 17:29:59 -0700

    Pass test to return the number of miles a user walked for a specific day

--- src/Activity.js
+++ src/Activity.js
@@ -2,6 +2,18 @@ class Activity {
   constructor(activityData) {
     this.activityData = activityData;
   }
+
+  getMilesWalkedForDay(id, date, stride) {
+  let activityPerUser = this.activityData.filter(activity => {
+    return activity.userID === id;
+  });
+
+  let activityPerDate = activityPerUser.find(activity => {
+    return activity.date === date;
+  });
+
+  return Number(((activityPerDate.numSteps * stride) / 5280).toFixed(1));
+  }
 }
 
 if (typeof module !== 'undefined') {
