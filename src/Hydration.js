45b3cbf678aca787841d204a4fb71fe2df810cc0
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-10 18:39:48 -0700

    Use filterRecords module method in Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,28 +4,22 @@ class Hydration {
     this.repoMethods = userRepoMethods;
   }
 
-  findDataForAUser(id) {
-    return this.hydrationData.filter(hydration => {
-      return hydration.userID === id;
-    });
-  }
-
   calcAvgOuncesConsumedForAllTime(id) {
-    const hydrationPerUser = this.findDataForAUser(id);
-    return this.repoMethods.findAverage(hydrationPerUser, 'numOunces', hydrationPerUser.length);
+    const userHydration = this.repoMethods.filterRecords(id, 'userID', this.hydrationData);
+    return this.repoMethods.findAverage(userHydration, 'numOunces', userHydration.length);
   }
 
   displayFluidOuncesConsumed(id, date) {
-    let hydrationPerUser = this.findDataForAUser(id)
-    let hydrationPerDate = hydrationPerUser.find(hydrationDate => {
+    let userHydration = this.repoMethods.filterRecords(id, 'userID', this.hydrationData);
+    let hydrationPerDate = userHydration.find(hydrationDate => {
       return hydrationDate.date === date;
     })
     return hydrationPerDate.numOunces;
   }
 
   findHydrationDataForAWeek(id, date) {
-    const hydrationForAUser = this.findDataForAUser(id);
-    return this.repoMethods.findDataForAGivenWeek(date, hydrationForAUser);
+    const userHydration = this.repoMethods.filterRecords(id, 'userID', this.hydrationData);
+    return this.repoMethods.findDataForAGivenWeek(date, userHydration);
   }
 }
 
