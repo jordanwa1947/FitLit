4fd6f53efe2f12115ec10d5a918fcc1897683df3
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-12 09:26:16 -0700

    Display average ounces with the ounces a user drank for the day

--- src/scripts.js
+++ src/scripts.js
@@ -82,9 +82,11 @@ function insertHydrationData() {
   const waterConsumed = document.getElementById('water-consumed');
   const waterOverAWeek = document.getElementById('water-over-a-week');
   const hydration = new Hydration(hydrationData, userRepository.repoMethods());
+  const avgOunces = hydration.calcAvgOuncesConsumedForAllTime(currentUser.id);
   const hydrationDataForAWeek = hydration.findHydrationDataForAWeek(currentUser.id, '2019/09/22');
   const todaysHydrationData = hydration.displayFluidOuncesConsumed(currentUser.id, '2019/09/22')
-  waterConsumed.innerHTML = `<p>Ounces Drank: ${todaysHydrationData}</p>`
+  waterConsumed.innerHTML = `<p>Ounces Drank: ${todaysHydrationData}</p>
+                             <p>Your Average: ${avgOunces}</p>`
   waterOverAWeek.innerHTML = formatHydrationDataForAWeek(hydrationDataForAWeek)
 }
 
